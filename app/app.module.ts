7af0dd5ca84f5976f4ea39b5be9e18be516f9b29
oleg_sukhov <oleg.sukhov@email.ua>
2016-10-09 23:39:45 +0300

    Add routing to the project

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HeroDetailComponent} from './hero-detail.component'
+import { HeroesComponent } from './heroes.component'
+import { RouterModule } from '@angular/router'
+import { DashboardComponent} from './dashboard.component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap: [ AppComponent]
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dash',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dash',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent,
+    DashboardComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ app/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-dash',
+    template: '<h3>Top Heroes Dashboard</h3>'
+})
+export class DashboardComponent {
+
+}
