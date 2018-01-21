e5e64350a73382daea3296bb951ac91172ff47b2
Oleg Sukhov <osukhov21@gmail.com>
2018-01-21 12:13:19 +0200

    NGA5-0001: Working with event via @ViewChild directive

--- ngAuction/src/app/search/search.component.ts
+++ ngAuction/src/app/search/search.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'nga-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('productTitle') productTitleField: ElementRef;
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    let keyup$ = Observable.fromEvent(this.productTitleField.nativeElement, 'keyup');
+    let keyupValue$ = keyup$
+    .debounceTime(500)
+    .map(event => event['target'].value)
+    .subscribe(productTitle => console.log(`Value ${productTitle}`))
   }
 
 }
