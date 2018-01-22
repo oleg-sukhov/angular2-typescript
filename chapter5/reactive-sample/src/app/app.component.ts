a85a94ca24fb6f1527ece2788913d3f4ccb5c276
Oleg Sukhov <osukhov21@gmail.com>
2018-01-22 08:55:29 +0200

    NGA-0005: Working with observable and RxJS

--- chapter5/reactive-sample/src/app/app.component.ts
+++ chapter5/reactive-sample/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import { FormControl } from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('eventObservable') eventInput: ElementRef;
+  formControlObservable: FormControl = new FormControl('')
+
+  constructor() {
+    this.formControlObservable.valueChanges
+      .debounceTime(700)
+      .subscribe(text => console.log(`From control text -> ${text}`));
+  }
+  public onKey({target}) : void {
+    console.log(`Typed -> ${target.value}`);
+  }
+
+  ngAfterViewInit() {
+    let keyup = Observable.fromEvent(this.eventInput.nativeElement, 'keyup');
+    let keyupValue = keyup
+      .debounceTime(800)
+      .map(event => event['target'].value)
+      .subscribe(text => console.log(`Text entered in observable input -> ${text}`));
+  }
 }
